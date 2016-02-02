014da9ecf20a1f91cf570313e5b78b57c9253bb9
chengluxi1818 <chengluxi@Luxis-MBP.att.net>
2016-02-02 10:13:06 -0800

    Done: 1. Mouseover the hidden words. 2. Mouseover the path with Javascript. Todo: 1. Clean up the javascript DOM document.getELement code.

--- jsscript.js
+++ jsscript.js
@@ -1,5 +1,30 @@
+/*var showHidden = document.getElementById('ShowHidden');
+
+var boss = document.getElementById('boss');
+var vp = document.getElementById('vp');
+var director = document.getElementById('director');
+var pm = document.getElementById('pm');
+var comrade = document.getElementById('comrade');
+
+var bossPath = document.getElementsByClassName('bossPath')[0];
+var vpPath = document.getElementsByClassName('vpPath')[0];
+var directorPath = document.getElementsByClassName('directorPath')[0];
+var pmPath = document.getElementsByClassName('pmPath')[0];
+var comradePath = document.getElementsByClassName('comradePath')[0];*/
+
+
 function init() {
+
 	document.getElementById('ShowHidden').style.visibility="hidden";
+
+	document.getElementsByClassName('bossPath')[0].style.visibility="hidden";
+	document.getElementsByClassName('vpPath')[0].style.visibility="hidden";
+	document.getElementsByClassName('directorPath')[0].style.visibility="hidden";
+	document.getElementsByClassName('pmPath')[0].style.visibility="hidden";
+	document.getElementsByClassName('comradePath')[0].style.visibility="hidden";
+
+	document.getElementsByClassName('leftHiddenWord')[0].style.visibility="hidden";
+	document.getElementsByClassName('rightHiddenWord')[0].style.visibility="hidden";
 }
 
 function show_hidden(index) {
@@ -8,28 +33,59 @@ function show_hidden(index) {
 	switch(index) {
 	case 0:
 		document.getElementById('ShowHidden').innerHTML = "You are boss";
+
 		document.getElementById('boss').style.backgroundColor = "black";
 		document.getElementById('boss').style.color = "white";
+
+		document.getElementsByClassName('bossPath')[0].style.visibility="visible";
+
+		document.getElementsByClassName('rightHiddenWord')[0].innerHTML="You are boss";
+		document.getElementsByClassName('rightHiddenWord')[0].style.visibility="visible";
+
 		break;
 	case 1:
 		document.getElementById('ShowHidden').innerHTML = "You are VP";
 		document.getElementById('vp').style.backgroundColor = "black";
 		document.getElementById('vp').style.color = "white";
+
+		document.getElementsByClassName('vpPath')[0].style.visibility="visible";
+
+		document.getElementsByClassName('leftHiddenWord')[0].innerHTML="You are vp";
+		document.getElementsByClassName('leftHiddenWord')[0].style.visibility="visible";
+
 		break;
 	case 2:
 		document.getElementById('ShowHidden').innerHTML = "You are Director";
 		document.getElementById('director').style.backgroundColor = "black";
 		document.getElementById('director').style.color = "white";
+
+		document.getElementsByClassName('directorPath')[0].style.visibility="visible";
+
+		document.getElementsByClassName('leftHiddenWord')[0].innerHTML="You are director";
+		document.getElementsByClassName('leftHiddenWord')[0].style.visibility="visible";
+
 		break;	
 	case 3:
 		document.getElementById('ShowHidden').innerHTML = "You are PM";
 		document.getElementById('pm').style.backgroundColor = "black";
 		document.getElementById('pm').style.color = "white";
+
+		document.getElementsByClassName('pmPath')[0].style.visibility="visible";
+
+		document.getElementsByClassName('rightHiddenWord')[0].innerHTML="You are pm";
+		document.getElementsByClassName('rightHiddenWord')[0].style.visibility="visible";
+
 		break;	
 	case 4:
 		document.getElementById('ShowHidden').innerHTML = "You are one of us";
-		document.getElementById('commerad').style.backgroundColor = "black";
-		document.getElementById('commerad').style.color = "white";		
+		document.getElementById('comrade').style.backgroundColor = "black";
+		document.getElementById('comrade').style.color = "white";
+
+		document.getElementsByClassName('comradePath')[0].style.visibility="visible";
+
+		document.getElementsByClassName('rightHiddenWord')[0].innerHTML="You are comrade";
+		document.getElementsByClassName('rightHiddenWord')[0].style.visibility="visible";
+
 		break;			
 	default:
 		document.getElementById('ShowHidden').innerHTML = "You are One of us";
@@ -43,25 +99,42 @@ function hide_hidden(index) {
 	case 0:
 		document.getElementById('boss').style.color="";
 		document.getElementById('boss').style.backgroundColor="";
+
+		document.getElementsByClassName('bossPath')[0].style.visibility="hidden";
+		document.getElementsByClassName('rightHiddenWord')[0].style.visibility="hidden";
+
 		break;
 	case 1:
 		document.getElementById('vp').style.color="";
 		document.getElementById('vp').style.backgroundColor="";
+
+		document.getElementsByClassName('vpPath')[0].style.visibility="hidden";
+		document.getElementsByClassName('leftHiddenWord')[0].style.visibility="hidden";
+
 		break;
 	case 2:
 		document.getElementById('director').style.color="";
 		document.getElementById('director').style.backgroundColor="";
+
+		document.getElementsByClassName('directorPath')[0].style.visibility="hidden";
+		document.getElementsByClassName('leftHiddenWord')[0].style.visibility="hidden";
 		break;	
 	case 3:
 		document.getElementById('pm').style.color="";
 		document.getElementById('pm').style.backgroundColor="";
+
+		document.getElementsByClassName('pmPath')[0].style.visibility="hidden";
+		document.getElementsByClassName('rightHiddenWord')[0].style.visibility="hidden";
 		break;	
 	case 4:
-		document.getElementById('commerad').style.color="";
-		document.getElementById('commerad').style.backgroundColor="";
+		document.getElementById('comrade').style.color="";
+		document.getElementById('comrade').style.backgroundColor="";
+
+		document.getElementsByClassName('comradePath')[0].style.visibility="hidden";
+		document.getElementsByClassName('rightHiddenWord')[0].style.visibility="hidden";
 		break;			
 	default:
-		document.getElementById('ShowHidden').innerHTML = "You are One of us";
+		showHidden.innerHTML = "You are One of us";
 	}
 
 
