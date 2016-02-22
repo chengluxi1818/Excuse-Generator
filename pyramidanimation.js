24200137249d80697ef663845df35307904362a4
chengluxi <luxi@virtualpowersystems.com>
2016-02-21 23:04:08 -0800

    Done: 1. Animation effect on paths, using JQuery, please see the 'pyramidanimation.js' file for details

--- pyramidanimation.js
+++ pyramidanimation.js
@@ -1,14 +1,42 @@
 $(document).ready(function() {
 	$(".boss").hover(function() {
-		$(".bossPath").toggle();
+		// $(".bossPath").toggle(function() {
+			$(".bossPath_1, .bossPath_2, .bossPath_3").each(function(fadeInDiv) {
+				$(this).delay(fadeInDiv * 500).fadeIn(1000);
+			});
+		// });
 	});
 
-	$(".vp").hover(function() {
-		$(".vpPath").toggle();
+	$(".vp").mouseenter(function() {
+		$(".vpPath").show();
+		$(".vpPath").animate({
+			// opacity: "toggle",
+			left: "800px",
+			right: "200px"
+		});
 	});
+	$(".vp").mouseleave(function() {
+		$(".vpPath").stop();
+	});
+
 
 	$(".director").hover(function() {
-		$(".directorPath").toggle();
+		var path = $(".directorPath");
+		var offset = path.offset();
+		var originRight = offset.right;
+
+		if(path.css("display") == "none") {
+			path.css("display", "block");
+			if(!path.is(".animated")) {
+				path.animate({
+					right: "200px"
+				});
+			}
+		}
+		else {
+			path.css('right', "98px");
+			path.css('display', "none");
+		}
 	});
 
 	$(".pm").hover(function() {
